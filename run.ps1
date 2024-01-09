e8729abefadfb419e7d3362df0d3292f28249a51
chris <jsoncode@qq.com>
2024-01-10 02:28:05 +0800

    update run.ps1 and remove BOM code

--- run.ps1
+++ run.ps1
@@ -1,12 +1,14 @@
 # @echo off
 # 转换文件格式
 $content = Get-Content -Raw -Path .\scripts\run_for_local.sh -Encoding utf8
-$content = $content -replace "`r`n", "`n"
-
+$content = $content -replace "`r$", ""
+# 移除 BOM
+$contentWithoutBOM = $content -replace '^\xEF\xBB\xBF', ''
 # 移除文件结尾的空白字符
-$content = $content -replace "\s+$", "`n`n"
+$content = $content -replace "\s+$", "`n`n`n"
 
-$content | Set-Content -Path .\scripts\run_for_local.sh -Encoding utf8
+# ([System.Text.Encoding]::UTF8) 可以禁止在开头添加bom
+$content | Set-Content -Path .\scripts\run_for_local.sh -Encoding ([System.Text.Encoding]::UTF8) -NoNewline
 
 # 判断Docker容器是否启动
 $dockerStatus = Get-Service | Where-Object {$_.DisplayName -like '*Docker*'} | Select-Object -ExpandProperty Status
