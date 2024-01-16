907fe6e52af1209ec99cd864a5bdf52c3c3957dd
xixihahaliu <xixihahaliu@github.com>
2024-01-16 18:41:27 +0800

    feat: Optimize the parsing effect and parsing speed of PDF.

--- qanything_kernel/dependent_server/ocr_serve/ocr_server.py
+++ qanything_kernel/dependent_server/ocr_serve/ocr_server.py
@@ -1,6 +1,12 @@
 from sanic import Sanic, response
 from paddleocr import PaddleOCR
-import sys
+import base64
+import numpy as np
+from sanic.worker.manager import WorkerManager
+import logging
+
+WorkerManager.THRESHOLD = 6000
+logging.basicConfig(level=logging.INFO)
 
 # 创建 Sanic 应用
 app = Sanic("OCRService")
@@ -8,22 +14,32 @@ app = Sanic("OCRService")
 # 初始化 PaddleOCR 引擎
 ocr_engine = PaddleOCR(use_angle_cls=True, lang="ch", use_gpu=True, show_log=False)
 
+
 # 定义 OCR API 路由
 @app.post("/ocr")
 async def ocr_request(request):
     # 获取上传的文件
-    img_file = request.files.get('file')
-    
+    input = request.json
+    img_file = input['img64']
+    height = input['height']
+    width = input['width']
+    channels = input['channels']
+
+    binary_data = base64.b64decode(img_file)
+    img_array = np.frombuffer(binary_data, dtype=np.uint8).reshape((height, width, channels))
+    logging.info("shape: {}".format(img_array.shape))
+
     # 无文件上传，返回错误
     if not img_file:
         return response.json({'error': 'No file was uploaded.'}, status=400)
 
     # 调用 PaddleOCR 进行识别
-    res = ocr_engine.ocr(img_file.body)
-    
+    res = ocr_engine.ocr(img_array)
+
     # 返回识别结果
     return response.json({'results': res})
 
+
+# 启动服务
 if __name__ == "__main__":
-    # 运行 Sanic 服务，使用 4 个 workers
-    app.run(host="0.0.0.0", port=8010, workers=4)
+    app.run(host="0.0.0.0", port=8010, workers=4, access_log=False)
