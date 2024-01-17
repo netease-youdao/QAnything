338748421166fb12650fdb00a63da852e760cd37
xixihahaliu <xixihahaliu@github.com>
2024-01-17 17:45:48 +0800

    chore: Optimize the startup script to improve usability.

--- scripts/run_for_local.sh
+++ scripts/run_for_local.sh
@@ -1,5 +1,44 @@
 ﻿#!/bin/bash
 
+env_file="/workspace/qanything_local/front_end/.env.production"
+user_file="/workspace/qanything_local/user.config"
+
+# 检查是否存在用户文件
+if [[ -f "$user_file" ]]; then
+    # 读取上次的配置
+    host=$(cat "$user_file")
+    read -p "Do you want to use the previous host $host? (yes/no) 是否使用上次的host $host？(是/否) " use_previous
+    if [[ $use_previous != "yes" && $use_previous != "是" ]]; then
+        read -p "Are you running the code on a cloud server or on your local machine? (cloud/local) 您是在云服务器上还是本地机器上启动代码？(云服务器/本地) " answer
+        if [[ $answer == "local" || $answer == "本地" ]]; then
+            host="localhost"
+        else
+            read -p "Please enter the server IP address 请输入服务器IP地址(示例：10.234.10.144): " host
+        fi
+        # 保存新的配置到用户文件
+        echo "$host" > "$user_file"
+    fi
+else
+    # 如果用户文件不存在，询问用户并保存配置
+    read -p "Are you running the code on a cloud server or on your local machine? (cloud/local) 您是在云服务器上还是本地机器上启动代码？(云服务器/本地) " answer
+    if [[ $answer == "local" || $answer == "本地" ]]; then
+        host="localhost"
+    else
+        read -p "Please enter the server IP address 请输入服务器IP地址 [x.x.x.x]: " host
+    fi
+    # 保存配置到用户文件
+    echo "$host" > "$user_file"
+fi
+
+# 保存IP地址到变量中
+api_host="http://$host:8777"
+
+# 使用 sed 命令更新 VITE_APP_API_HOST 的值
+sed -i "s|VITE_APP_API_HOST=.*|VITE_APP_API_HOST=$api_host|" "$env_file"
+
+echo "The file $env_file has been updated with the following configuration:"
+grep "VITE_APP_API_HOST" "$env_file"
+
 start_time=$(date +%s)  # 记录开始时间
 # 检查模型文件夹是否存在
 check_folder_existence() {
@@ -58,19 +97,20 @@ fi
 CUDA_VISIBLE_DEVICES=0 nohup /opt/tritonserver/bin/tritonserver --model-store=/model_repos/QAEnsemble_base --http-port=10000 --grpc-port=10001 --metrics-port=10002 --log-verbose=$VERBOSE > /model_repos/QAEnsemble_base/QAEnsemble_base.log 2>&1 &
 CUDA_VISIBLE_DEVICES=0 nohup /opt/tritonserver/bin/tritonserver --model-store=/model_repos/QAEnsemble_embed --http-port=9000 --grpc-port=9001 --metrics-port=9002 --log-verbose=$VERBOSE > /model_repos/QAEnsemble_embed/QAEnsemble_embed.log 2>&1 &
 CUDA_VISIBLE_DEVICES=0 nohup /opt/tritonserver/bin/tritonserver --model-store=/model_repos/QAEnsemble_rerank --http-port=8000 --grpc-port=8001 --metrics-port=8002 --log-verbose=$VERBOSE > /model_repos/QAEnsemble_rerank/QAEnsemble_rerank.log 2>&1 &
-cd /workspace/qanything_local || exit
-CUDA_VISIBLE_DEVICES=0 nohup python3 -u qanything_kernel/dependent_server/ocr_serve/ocr_server.py > ocr.log 2>&1 &
-echo "The ocr service is ready! (3/8)"
-echo "OCR服务已就绪! (3/8)"
 
-# cd /workspace/qanything_local/qanything_kernel/dependent_server/llm_for_local_serve || exit
+cd /workspace/qanything_local/qanything_kernel/dependent_server/llm_for_local_serve || exit
 nohup python3 -u llm_server_entrypoint.py --host="0.0.0.0" --port=36001 --model-path="tokenizer_assets" --model-url="0.0.0.0:10001" > llm.log 2>&1 &
 echo "The llm transfer service is ready! (1/8)"
 echo "大模型中转服务已就绪! (1/8)"
 
+cd /workspace/qanything_local || exit
 nohup python3 -u qanything_kernel/dependent_server/rerank_for_local_serve/rerank_server.py > rerank.log 2>&1 &
 echo "The rerank service is ready! (2/8)"
 echo "rerank服务已就绪! (2/8)"
+
+CUDA_VISIBLE_DEVICES=0 nohup python3 -u qanything_kernel/dependent_server/ocr_serve/ocr_server.py > ocr.log 2>&1 &
+echo "The ocr service is ready! (3/8)"
+echo "OCR服务已就绪! (3/8)"
 
 nohup python3 -u qanything_kernel/qanything_server/sanic_api.py > api.log 2>&1 &
 echo "The qanything backend service is ready! (4/8)"
@@ -82,9 +122,13 @@ cd /workspace/qanything_local/front_end || exit
 
 # 安装依赖
 echo "Waiting for [npm run install]（5/8)"
-npm install
+timeout_time=300
+timeout $timeout_time npm install
 if [ $? -eq 0 ]; then
     echo "[npm run install] Installed successfully（5/8)"
+elif [ $? -eq 124 ]; then
+    echo "npm install 下载超时，可能是网络问题，请修改 npm 代理。"
+    exit 1
 else
     echo "Failed to install npm dependencies."
     exit 1
@@ -105,10 +149,23 @@ nohup npm run serve 1>npm_server.log 2>&1 &
 
 # 监听前端页面服务
 tail -f npm_server.log &
+
+front_end_start_time=$(date +%s)
+
 while ! grep -q "Local:" npm_server.log; do
     echo "Waiting for the front-end service to start..."
     echo "等待启动前端服务"
     sleep 1
+
+    # 获取当前时间并计算经过的时间
+    current_time=$(date +%s)
+    elapsed_time=$((current_time - front_end_start_time))
+
+    # 检查是否超时
+    if [ $elapsed_time -ge 120 ]; then
+        echo "启动前端服务超时，请检查日志文件 front_end/npm_server.log 获取更多信息。"
+        exit 1
+    fi
 done
 echo "The front-end service is ready!...(7/8)"
 echo "前端服务已就绪!...(7/8)"
@@ -127,6 +184,15 @@ while true; do
   else
     echo "The triton service is starting up, it can be long... you have time to make a coffee :)"
     echo "Triton服务正在启动，可能需要一段时间...你有时间去冲杯咖啡 :)"
+
+    current_time=$(date +%s)
+    elapsed_time=$((current_time - start_time))
+
+    # 检查是否超时
+    if [ $elapsed_time -ge $((timeout_time * 2)) ]; then
+        echo "启动Triton服务超时，请进入容器内检查/model_repos/QAEnsemble_base/QAEnsemble_base.log以获取更多信息。"
+        exit 1
+    fi
     sleep 5
   fi
 done
@@ -135,8 +201,8 @@ current_time=$(date +%s)
 elapsed=$((current_time - start_time))  # 计算经过的时间（秒）
 echo "Time elapsed: ${elapsed} seconds."
 echo "已耗时: ${elapsed} 秒."
-echo "Please visit the front-end service at [http://localhost:5052/qanything/] to conduct Q&A. Please replace "localhost" with the actual IP address according to the actual situation."
-echo "请在[http://localhost:5052/qanything/]下访问前端服务来进行问答，请根据实际情况将localhost替换为实际ip"
+echo "Please visit the front-end service at [http://$host:5052/qanything/] to conduct Q&A."
+echo "请在[http://$host:5052/qanything/]下访问前端服务来进行问答，如果前端报错，请在浏览器按F12以获取更多报错信息"
 
 # 保持容器运行
 while true; do
