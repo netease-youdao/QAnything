446201411f658195a34dc04546aa470ff1e547be
dongfeng <dongfeng@51ddi.com>
2024-01-25 18:43:07 +0800

    Update request max size code annotation

--- qanything_kernel/qanything_server/sanic_api.py
+++ qanything_kernel/qanything_server/sanic_api.py
@@ -69,7 +69,7 @@ if args.mode not in ['local', 'online']:
     raise ValueError('mode must be local or online')
 
 app = Sanic("QAnything")
-# 设置请求体最大为 10MB
+# 设置请求体最大为 400MB
 app.config.REQUEST_MAX_SIZE = 400 * 1024 * 1024
 
 # CORS中间件，用于在每个响应中添加必要的头信息
