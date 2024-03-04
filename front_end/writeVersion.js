b693f651055b73c02271e5222282cb36768cb01d
xixihahaliu <xixihahaliu01@gmail.com>
2024-03-04 16:57:02 +0800

    chore: Delete useless files.

--- front_end/writeVersion.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-try {
-    const data = fs.readFileSync('./package.json', {encoding: 'utf-8'});
-    const jsonData = JSON.parse(data);
-    fs.writeFileSync('./version.txt', jsonData.version, {encoding: 'utf-8'})
-} catch (error) {
-    console.error(`无法读取 package.json:`, error);
-}
