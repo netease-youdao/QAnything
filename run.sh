5162e1d1a0602f1dccb878a203cb4cf25c6b3dd7
beat4ocean <beat4ocean@gmail.com>
2024-03-29 11:37:43 +0800

    fix "docker compose" command bug

--- run.sh
+++ run.sh
@@ -274,18 +274,18 @@ if [ -e /proc/version ]; then
         echo "Running under git bash"
     fi
     
-    if docker-compose -p user -f docker-compose-windows.yaml down |& tee /dev/tty | grep -q "services.qanything_local.deploy.resources.reservations value 'devices' does not match any of the regexes"; then
-        echo "检测到 Docker Compose 版本过低，请升级到v2.23.3或更高版本。执行docker-compose -v查看版本。"
+    if docker compose -p user -f docker compose-windows.yaml down |& tee /dev/tty | grep -q "services.qanything_local.deploy.resources.reservations value 'devices' does not match any of the regexes"; then
+        echo "检测到 Docker Compose 版本过低，请升级到v2.23.3或更高版本。执行docker compose -v查看版本。"
     fi
-    docker-compose -p user -f docker-compose-windows.yaml up -d
-    docker-compose -p user -f docker-compose-windows.yaml logs -f qanything_local
+    docker compose -p user -f docker compose-windows.yaml up -d
+    docker compose -p user -f docker compose-windows.yaml logs -f qanything_local
   else
     echo "Running under native Linux"
-    if docker-compose -p user -f docker-compose-linux.yaml down |& tee /dev/tty | grep -q "services.qanything_local.deploy.resources.reservations value 'devices' does not match any of the regexes"; then
-        echo "检测到 Docker Compose 版本过低，请升级到v2.23.3或更高版本。执行docker-compose -v查看版本。"
+    if docker compose -p user -f docker compose-linux.yaml down |& tee /dev/tty | grep -q "services.qanything_local.deploy.resources.reservations value 'devices' does not match any of the regexes"; then
+        echo "检测到 Docker Compose 版本过低，请升级到v2.23.3或更高版本。执行docker compose -v查看版本。"
     fi
-    docker-compose -p user -f docker-compose-linux.yaml up -d
-    docker-compose -p user -f docker-compose-linux.yaml logs -f qanything_local
+    docker compose -p user -f docker compose-linux.yaml up -d
+    docker compose -p user -f docker compose-linux.yaml logs -f qanything_local
     # 检查日志输出
   fi
 else
