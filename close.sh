908724f41a01fcfae9c0c4fa886150b3bc97d68b
beat4ocean <beat4ocean@gmail.com>
2024-03-29 11:53:55 +0800

    fix "docker compose" command bug

--- close.sh
+++ close.sh
@@ -7,10 +7,10 @@ if [ -e /proc/version ]; then
     else
         echo "Running under git bash"
     fi
-    docker-compose -p user -f docker-compose-windows.yaml down
+    docker compose -p user -f docker-compose-windows.yaml down
   else
     echo "Running under native Linux"
-    docker-compose -p user -f docker-compose-linux.yaml down
+    docker compose -p user -f docker-compose-linux.yaml down
   fi
 else
   echo "/proc/version 文件不存在。请确认自己位于Linux或Windows的WSL环境下"
