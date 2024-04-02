499dcdab60c2afafe8632f52535fe4c917ec90bc
xixihahaliu <xixihahaliu01@gmail.com>
2024-04-02 19:11:31 +0800

    fix: Incorrect startup script parameters.

--- scripts/run_for_openai_api_in_M1_mac.sh
+++ scripts/run_for_openai_api_in_M1_mac.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bash scripts/base_run.sh -s "M1mac" -w 4 -m 19530 -q 8777 -o -b 'https://api.openai-proxy.org/v1' -k 'sk-AM3RP0lUEoGRRaJBTR6o0pPGs5FIiGZ6UMXC43Q6D3eQABHl' -n 'gpt-3.5-turbo-1106' -l '4096'
+bash scripts/base_run.sh -s "M1mac" -w 4 -m 19530 -q 8777 -o -b 'https://api.openai.com/v1' -k 'sk-xxx' -n 'gpt-3.5-turbo-1106' -l '4096'
