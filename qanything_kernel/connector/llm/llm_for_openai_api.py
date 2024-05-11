4804041381aea8985354a4ccfba3d6cb37d8fa41
xixihahaliu <xixihahaliu01@gmail.com>
2024-05-11 17:07:46 +0800

    fix: Support more open source model APIs compatible with OpenAI interfaces.

--- qanything_kernel/connector/llm/llm_for_openai_api.py
+++ qanything_kernel/connector/llm/llm_for_openai_api.py
@@ -68,18 +68,15 @@ class OpenAILLM(BaseAnswer, ABC):
         # 针对不同的模型设置token数量
         if model in {
             "gpt-3.5-turbo-0613",
-            # "gpt-3.5-turbo-1106",
+            "gpt-3.5-turbo-1106",
             "gpt-3.5-turbo-16k-0613",
             "gpt-4-0314",
             "gpt-4-32k-0314",
             "gpt-4-0613",
             "gpt-4-32k-0613",
             "gpt-4-32k",
-            # "gpt-4-1106-preview",
-            "Qwen1.5-72B-Chat",
-            "Qwen1.5-14B-Chat",
-            "Qwen1.5-7B-Chat",
-            }:
+            "gpt-4-1106-preview",
+        }:
             tokens_per_message = 3
             tokens_per_name = 1
         elif model == "gpt-3.5-turbo-0301":
@@ -87,18 +84,18 @@ class OpenAILLM(BaseAnswer, ABC):
             tokens_per_name = -1  # 如果有名字，角色会被省略
         elif "gpt-3.5-turbo" in model:
             # 对于 gpt-3.5-turbo 模型可能会有更新，此处返回假设为 gpt-3.5-turbo-0613 的token数量，并给出警告
-            logging.info("Warning: gpt-3.5-turbo may update over time. Returning num tokens assuming gpt-3.5-turbo-0613.")
+            logging.info(
+                "Warning: gpt-3.5-turbo may update over time. Returning num tokens assuming gpt-3.5-turbo-0613.")
             return self.num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613")
         elif "gpt-4" in model:
             # 对于 gpt-4 模型可能会有更新，此处返回假设为 gpt-4-0613 的token数量，并给出警告
             logging.info("Warning: gpt-4 may update over time. Returning num tokens assuming gpt-4-0613.")
             return self.num_tokens_from_messages(messages, model="gpt-4-0613")
-
         else:
-            # 对于没有实现的模型，抛出未实现错误
-            raise NotImplementedError(
-                f"""num_tokens_from_messages() is not implemented for model {model}. See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens."""
-            )
+            # 对于 其他 模型可能会有更新，此处返回假设为 gpt-3.5-turbo-1106 的token数量，并给出警告
+            logging.info(
+                f"Warning: {model} may update over time. Returning num tokens assuming gpt-3.5-turbo-1106.")
+            return self.num_tokens_from_messages(messages, model="gpt-3.5-turbo-1106")
             
         num_tokens = 0
         # 计算每条消息的token数
