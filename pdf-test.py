5d85819713bfd27d60f1b5fdd288f443ec7c7520
Mo Xiaofei <126770734+ikun-moxiaofei@users.noreply.github.com>
2024-06-12 15:36:31 +0800

    Delete pdf-test.py

--- pdf-test.py
+++
@@ -1,9 +0,0 @@
-from qanything_kernel.utils.loader import PdfLoader
-
-# 创建 PdfLoader 类的实例
-# 可以自由更改路径
-pdf_loader = PdfLoader(filename='/Users/admin/QA/QAnything/xxx.pdf', save_dir_='/Users/admin/QA/QAnything/')
-
-# 调用 load_to_markdown 方法进行转换
-markdown_directory = pdf_loader.load_to_markdown()
-print(f"Markdown文件在: {markdown_directory}")
