7c28414ee1b6905d18c9b08b4971a4dceae56ada
xixihahaliu <xixihahaliu01@gmail.com>
2024-08-27 14:58:58 +0800

    DEL: Unless file.

--- scripts/run_for_openai_api_in_M1_mac.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-bash scripts/base_run.sh -s "M1mac" -w 4 -m 19530 -q 8777 -o -b 'https://api.openai-proxy.org/v1' -k 'sk-ZY6CX9d4c3SJjd3AGSXyuG8UVIKVix729bWKiROLXT5CJofv@29189' -n 'gpt-3.5-turbo-1106' -l '4096'
--- scripts/run_for_openai_api_with_cpu_in_Linux_or_WSL.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-bash scripts/base_run.sh -s "LinuxOrWSL" -w 4 -m 19530 -q 8777 -c -o -b 'https://api.openai.com/v1' -k 'sk-xxx' -n 'gpt-3.5-turbo-1106' -l '4096'
